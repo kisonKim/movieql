9b7fb1432837e65b4deae1a81cdd1841b5ab6d30
kisonKim <npolly7@gmail.com>
2021-05-17 16:23:19 +0900

    finished

---
+++ graphql/resolvers.js
@@ -0,0 +1,9 @@
+import {getMovies, addMovie, deleteMovie, getById} from "./db";
+
+const resolvers = {
+  Query: {
+    movies: (_,{limit,rating}) => getMovies(limit,rating),
+  }
+}
+
+export default resolvers; 
